1e688fe299b36594e9d2b6a68b5e793cedf34e87
SarcasticNickname <sarcasticnickname@protonmail.com>
2021-06-20 10:00:01 +0300

    Merge sort

---
+++ MergeSort/src/main/MergeSort.java
@@ -0,0 +1,51 @@
+package main;
+
+/**
+ * @author SarcasticNickname
+ */
+public class MergeSort implements Sort {
+	
+	private void merge(int [] num,int left,int mid, int right) {
+		int length  = right - left + 1;
+		int [] param = new int[num.length];
+		
+		int lindx = left;
+		int rindx = mid + 1;
+		int pindx = 0;
+		
+		while(lindx <= mid && rindx <= right) {
+			if(num[lindx] < num[rindx])
+				param[pindx++] = num[lindx++];
+			else
+				param[pindx++] = num[rindx++];
+		}
+		
+		while(lindx <= mid) {
+			param[pindx++] = num[lindx++];
+		}
+		
+		while(rindx <= right) {
+			param[pindx++] = num[rindx++]; 
+		}
+		System.arraycopy(param, 0, num, left, length);
+	}
+
+	public void doSort(int[] num,int left, int right) {
+		if(left >= right) 
+			return;
+		//Оператор сдвига на бит вправо без учёта знака,
+		//Но у нас же индексы, не может быть отрицательного
+		//По сути деление на 2(ещё и -1 для нечётных)
+		int mid = (left+right) >>> 1;
+		
+		doSort(num, left, mid);
+		doSort(num, mid+1, right);
+		merge(num, left, mid, right);
+	}
+	
+	@Override
+	public int[] sort(int[] num) {
+		doSort(num, 0, num.length - 1);
+		return num;
+	}
+}
---
+++ MergeSort/src/main/Sort.java
@@ -0,0 +1,5 @@
+package main;
+
+public interface Sort {
+	int [] sort(int [] num);
+}
